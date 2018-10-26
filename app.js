2fa608e31a7f1ea8d13f34477b180e80cd92b6e9
Jody Isaguirre <jodyisaguirre@Jodys-MacBook-Air.local>
2018-10-26 13:44:31 -0600

    added error handling

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const students = ('./studens.json')
 app.get ('/', (req,res) =>{
   res.send('Soft as cake 🍰 ')
 })
+
+
+
+app.use(notFound)
+  // General purpose 'catch' all errors
+app.use(errorHandler)
+
+
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
